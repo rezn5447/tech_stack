cd6a6a7eed6c39beebd895d2199d833d3cdc5092
Resdan Logan <rezn5447@gmail.com>
2017-02-26 07:37:07 -0800

    Initial Commit

---
+++ src/components/BoilerPlate.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+class Name extends Component {
+  render() {
+    return (
+      <View />
+    );
+  }
+}
+
+export default Name;
